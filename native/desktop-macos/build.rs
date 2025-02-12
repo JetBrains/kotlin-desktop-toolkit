3fbb8badcb510d9d362ac74183e37c8c3a37ad68
pavel.sergeev <sergeev.pavel.al@gmail.com>
2025-02-12 15:56:23 +0100

    set install name for dylib
    we set it this way because are going to bundle it with application and not going to install it in system folder

--- native/desktop-macos/build.rs
+++ native/desktop-macos/build.rs
@@ -4,6 +4,12 @@ use std::env;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    // Get the crate name from Cargo environment variable
+
+    let artifact_name = env::var("CARGO_PKG_NAME").unwrap().replace("-", "_");
+    let dylib_name = format!("lib{artifact_name}.dylib");
+    let header_name = format!("{artifact_name}.h");
+    println!("cargo:rustc-link-arg=-Wl,-install_name,./{dylib_name}");
 
     cbindgen::generate(crate_dir)
 //      .expect("Unable to generate bindings")
@@ -14,7 +20,7 @@ fn main() {
             e => panic!("{:?}", e),
         },
         |bindings| {
-            bindings.write_to_file("headers/desktop_macos.h");
+            bindings.write_to_file(format!("headers/{header_name}"));
         },
     );
     unsafe {
