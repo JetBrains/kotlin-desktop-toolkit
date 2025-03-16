d540f48b8b88789d2ad3e3606786a5af415d2a68
pavel.sergeev <sergeev.pavel.al@gmail.com>
2025-03-16 23:12:33 +0100

    linux crate

---
+++ native/desktop-linux/build.rs
@@ -0,0 +1,31 @@
+extern crate cbindgen;
+
+use std::env;
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    // Get the crate name from Cargo environment variable
+
+    let artifact_name = env::var("CARGO_PKG_NAME").unwrap().replace('-', "_");
+//    let dylib_name = format!("lib{artifact_name}.so");
+    let header_name = format!("{artifact_name}.h");
+//    println!("cargo:rustc-link-arg=-Wl,-install_name,./{dylib_name}");
+
+    cbindgen::generate(crate_dir)
+        //.expect("Unable to generate bindings")
+        //.write_to_file("headers/desktop_macos.h");
+        .map_or_else(
+            |error| match error {
+                e @ cbindgen::Error::ParseSyntaxError { .. } => {
+                    eprintln!("Parse error: {e:?}");
+                }
+                e => panic!("{e:?}"),
+            },
+            |bindings| {
+                bindings.write_to_file(format!("headers/{header_name}"));
+            },
+        );
+    unsafe {
+        env::set_var("RUSTC_BOOTSTRAP", "0");
+    }
+}
---
+++ native/desktop-linux/headers/desktop_linux.h
@@ -0,0 +1,6 @@
+/* This header is generated please don't edit it manually. */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+uint64_t add(uint64_t left, uint64_t right);
