f2beb537674195bd756d7a1db30931382ab18163
Nikola Kocić <nikola.kocic@jetbrains.com>
2025-03-24 13:52:04 +0100

    linux: add keyboard events

--- kotlin-desktop-toolkit-linux/src/main/kotlin/org/jetbrains/desktop/linux/Converters.kt
+++ kotlin-desktop-toolkit-linux/src/main/kotlin/org/jetbrains/desktop/linux/Converters.kt
@@ -1,16 +1,15 @@
 package org.jetbrains.desktop.linux
 
+import org.jetbrains.desktop.linux.generated.NativeKeyModifiers
 import org.jetbrains.desktop.linux.generated.NativeLogicalPoint
 import org.jetbrains.desktop.linux.generated.NativeLogicalSize
 import java.lang.foreign.Arena
 import java.lang.foreign.MemorySegment
 
-internal fun LogicalSize.Companion.fromNative(s: MemorySegment): LogicalSize {
-    return LogicalSize(
-        width = NativeLogicalSize.width(s),
-        height = NativeLogicalSize.height(s),
-    )
-}
+internal fun LogicalSize.Companion.fromNative(s: MemorySegment) = LogicalSize(
+    width = NativeLogicalSize.width(s),
+    height = NativeLogicalSize.height(s),
+)
 
 internal fun LogicalSize.toNative(arena: Arena): MemorySegment {
     val result = NativeLogicalSize.allocate(arena)
@@ -19,12 +18,10 @@ internal fun LogicalSize.toNative(arena: Arena): MemorySegment {
     return result
 }
 
-internal fun LogicalPoint.Companion.fromNative(s: MemorySegment): LogicalPoint {
-    return LogicalPoint(
-        x = NativeLogicalPoint.x(s),
-        y = NativeLogicalPoint.y(s),
-    )
-}
+internal fun LogicalPoint.Companion.fromNative(s: MemorySegment) = LogicalPoint(
+    x = NativeLogicalPoint.x(s),
+    y = NativeLogicalPoint.y(s),
+)
 
 internal fun LogicalPoint.toNative(arena: Arena): MemorySegment {
     val result = NativeLogicalPoint.allocate(arena)
@@ -32,3 +29,12 @@ internal fun LogicalPoint.toNative(arena: Arena): MemorySegment {
     NativeLogicalPoint.y(result, y)
     return result
 }
+
+internal fun KeyModifiers.Companion.fromNative(s: MemorySegment) = KeyModifiers(
+    capsLock = NativeKeyModifiers.caps_lock(s),
+    shift = NativeKeyModifiers.shift(s),
+    control = NativeKeyModifiers.ctrl(s),
+    alt = NativeKeyModifiers.alt(s),
+    logo = NativeKeyModifiers.logo(s),
+    numLock = NativeKeyModifiers.num_lock(s),
+)
--- native/desktop-linux/src/linux/keyboard.rs
+++ native/desktop-linux/src/linux/keyboard.rs
@@ -1,7 +1,7 @@
+use std::{ffi::CString, str::FromStr};
+
 use log::debug;
 use smithay_client_toolkit::seat::keyboard::{KeyEvent, Keysym, Modifiers};
-
-use crate::linux::cursors::CURSORS;
 
 use super::{events::Event, window::SimpleWindow};
 
@@ -18,29 +18,46 @@ impl SimpleWindow {
     }
 
     pub fn press_key(&mut self, event: &KeyEvent) {
-        if event.keysym == Keysym::N {
-            // Cycle through cursor icons.
-            self.window_cursor_icon_idx = (self.window_cursor_icon_idx + 1) % CURSORS.len();
-            debug!("Setting cursor icon to: {}", CURSORS[self.window_cursor_icon_idx].name());
-            self.set_cursor = true;
-        }
+        let characters = event.utf8.as_ref().map(|s| CString::from_str(s).unwrap());
+        let key = event.keysym.name().map(|s| CString::from_str(s).unwrap());
+        (self.event_handler)(&Event::new_key_down_event(event, characters.as_ref(), key.as_ref()));
     }
 
-    pub fn release_key(&mut self, _event: &KeyEvent) {}
+    pub fn release_key(&mut self, event: &KeyEvent) {
+        let characters = event.utf8.as_ref().map(|s| CString::from_str(s).unwrap());
+        let key = event.keysym.name().map(|s| CString::from_str(s).unwrap());
+        (self.event_handler)(&Event::new_key_up_event(event, characters.as_ref(), key.as_ref()));
+    }
 
-    pub fn update_modifiers(&mut self, _: Modifiers, _layout: u32) {}
+    pub fn update_modifiers(&mut self, modifiers: Modifiers, _layout: u32) {
+        (self.event_handler)(&Event::new_modifiers_changed_event(modifiers));
+    }
 }
 
-#[derive(Clone, Copy)]
-#[repr(transparent)]
-pub struct KeyModifiersSet(pub usize);
+#[derive(Clone, Copy, Debug, Default)]
+#[repr(C)]
+pub struct KeyModifiers {
+    /// The "control" key
+    pub ctrl: bool,
 
-impl std::fmt::Debug for KeyModifiersSet {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "KeyModifiersSet({:032b})", self.0)
-    }
+    /// The "alt" key
+    pub alt: bool,
+
+    /// The "shift" key
+    pub shift: bool,
+
+    /// The "Caps lock" key
+    pub caps_lock: bool,
+
+    /// The "logo" key
+    ///
+    /// Also known as the "windows" or "super" key on a keyboard.
+    pub logo: bool,
+
+    /// The "Num lock" key
+    pub num_lock: bool,
 }
 
 #[repr(transparent)]
 #[derive(Debug, Clone, Copy)]
-pub struct KeyCode(pub u16);
+pub struct KeyCode(pub u32);
