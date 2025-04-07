6ba992d3f328e491a982f7fbec7c86335fd4da26
Nikola Kocić <nikola.kocic@jetbrains.com>
2025-04-07 19:59:50 +0200

    linux: multiple windows support

--- native/desktop-linux/src/bin/desktop-linux-test/wayland.rs
+++ native/desktop-linux/src/bin/desktop-linux-test/wayland.rs
@@ -1,5 +1,17 @@
-use desktop_linux::test_main;
+use desktop_common::{
+    ffi_utils::BorrowedStrPtr,
+    logger_api::{LogLevel, LoggerConfiguration, logger_init_impl},
+};
+use desktop_linux::linux::application::Application;
 
 pub fn main() {
-    test_main();
+    logger_init_impl(&LoggerConfiguration {
+        file_path: BorrowedStrPtr::new(c"/tmp/a"),
+        console_level: LogLevel::Debug,
+        file_level: LogLevel::Error,
+    });
+    let mut app = Application::new().unwrap();
+    app.new_window();
+    app.new_window();
+    app.run();
 }
