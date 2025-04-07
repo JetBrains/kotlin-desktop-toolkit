6ceaccdafe510be392c10538e5f810e9031181a6
Nikola Kocić <nikola.kocic@jetbrains.com>
2025-04-07 15:19:15 +0200

    linux: start implementing events

--- native/desktop-linux/src/linux/keyboard.rs
+++ native/desktop-linux/src/linux/keyboard.rs
@@ -2,16 +2,18 @@ use smithay_client_toolkit::seat::keyboard::{KeyEvent, Keysym, Modifiers};
 
 use crate::linux::cursors::CURSORS;
 
-use super::window::SimpleWindow;
+use super::{events::Event, window::SimpleWindow};
 
 impl SimpleWindow {
     pub fn keyboard_enter(&mut self, keysyms: &[Keysym]) {
         println!("Keyboard focus on window with pressed syms: {keysyms:?}");
         self.keyboard_focus = true;
+        (self.event_handler)(&Event::new_window_focus_change_event(true));
     }
 
     pub fn keyboard_leave(&mut self) {
         self.keyboard_focus = false;
+        (self.event_handler)(&Event::new_window_focus_change_event(false));
     }
 
     pub fn press_key(&mut self, event: &KeyEvent) {
