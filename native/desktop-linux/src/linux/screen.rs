c1f58a83e6a4a4d6e59acaea726a8c2ad782f821
Nikola Kocić <nikola.kocic@jetbrains.com>
2025-04-08 15:31:45 +0200

    linux: add Screen.maximumFramesPerSecond

--- native/desktop-linux/src/linux/screen.rs
+++ native/desktop-linux/src/linux/screen.rs
@@ -2,7 +2,7 @@ use desktop_common::{
     ffi_utils::{AutoDropArray, AutoDropStrPtr, RustAllocatedStrPtr},
     logger::ffi_boundary,
 };
-use smithay_client_toolkit::output::OutputInfo;
+use smithay_client_toolkit::output::{Mode, OutputInfo};
 
 use super::{
     application::Application,
@@ -21,13 +21,20 @@ pub struct ScreenInfo {
     pub origin: LogicalPoint,
     pub size: LogicalSize,
     pub scale: f64,
+    pub maximum_frames_per_second: i32,
     // todo color space?
     // todo stable uuid?
 }
 
 impl ScreenInfo {
+    #[allow(clippy::cast_possible_truncation)]
+    fn get_refresh_rate_fps(mode: &Mode) -> i32 {
+        (f64::from(mode.refresh_rate) / 1000.).round() as i32
+    }
+
     #[must_use]
     pub fn new(info: OutputInfo) -> Self {
+        let current_mode = info.modes.iter().find(|m| m.current);
         Self {
             screen_id: info.id,
             // The screen containing the menu bar is always the first object (index 0) in the array returned by the screens method.
@@ -54,6 +61,7 @@ impl ScreenInfo {
                 })
                 .unwrap_or_default(),
             scale: info.scale_factor.into(),
+            maximum_frames_per_second: current_mode.map(Self::get_refresh_rate_fps).unwrap_or_default(),
         }
     }
 }
