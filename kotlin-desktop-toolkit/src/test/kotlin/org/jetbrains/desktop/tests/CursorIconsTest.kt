2de16941c1024b9750b1b098832576f161f34f2c
pavel.sergeev <sergeev.pavel.al@gmail.com>
2025-05-26 21:51:17 +0200

    tests hacky fix

--- kotlin-desktop-toolkit/src/test/kotlin/org/jetbrains/desktop/tests/CursorIconsTest.kt
+++ kotlin-desktop-toolkit/src/test/kotlin/org/jetbrains/desktop/tests/CursorIconsTest.kt
@@ -1,8 +1,9 @@
-import org.jetbrains.desktop.macos.Application
 import org.jetbrains.desktop.macos.Cursor
 import org.jetbrains.desktop.macos.GrandCentralDispatch
 import org.jetbrains.desktop.macos.KotlinDesktopToolkit
 import org.jetbrains.desktop.macos.LogLevel
+import org.jetbrains.desktop.macos.LogicalPoint
+import org.jetbrains.desktop.macos.Window
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -10,8 +11,11 @@ class CursorIconsTest {
     @Test
     fun interateCursorIconsTest() {
         KotlinDesktopToolkit.init(consoleLogLevel = LogLevel.Info)
-        GrandCentralDispatch.dispatchOnMainSync {
-            Application.init()
+//        GrandCentralDispatch.dispatchOnMainSync {
+//            Application.init()
+//        }
+        val window1 = GrandCentralDispatch.dispatchOnMainSync {
+            Window.create(origin = LogicalPoint(100.0, 200.0), title = "Hello1")
         }
         Cursor.Icon.entries.forEach { icon ->
             GrandCentralDispatch.dispatchOnMainSync {
@@ -21,6 +25,9 @@ class CursorIconsTest {
                 Cursor.icon
             }
             assertEquals(actualIcon, icon)
+        }
+        GrandCentralDispatch.dispatchOnMainSync {
+            window1.close()
         }
     }
 
