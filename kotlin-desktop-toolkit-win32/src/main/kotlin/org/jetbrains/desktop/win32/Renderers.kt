885d8f81e29b7cefd741f8f1f5d7b3fe5dc3b473
Alexander Ovchinnikov <alexander.ovchinnikov@jetbrains.com>
2025-06-26 16:53:53 +0200

    ANGLE renderer

---
+++ kotlin-desktop-toolkit-win32/src/main/kotlin/org/jetbrains/desktop/win32/Renderers.kt
@@ -0,0 +1,55 @@
+package org.jetbrains.desktop.win32
+
+import org.jetbrains.desktop.win32.generated.NativeAngleDeviceCallbacks
+import org.jetbrains.desktop.win32.generated.NativeAngleDeviceDrawFun
+import org.jetbrains.desktop.win32.generated.NativeEglGetProcFuncData
+import org.jetbrains.desktop.win32.generated.desktop_windows_h
+import java.lang.foreign.Arena
+import java.lang.foreign.MemorySegment
+
+public class AngleRenderer internal constructor(
+    private val angleDevicePtr: MemorySegment,
+) : AutoCloseable {
+    public companion object {
+        public fun create(window: Window): AngleRenderer {
+            return AngleRenderer(
+                ffiDownCall {
+                    window.withPointer { windowPtr ->
+                        desktop_windows_h.renderer_angle_device_create(windowPtr)
+                    }
+                },
+            )
+        }
+    }
+
+    public data class EglGetProcFunc(val fPtr: Long, val ctxPtr: Long)
+
+    public fun getEglGetProcFunc(): EglGetProcFunc {
+        return Arena.ofConfined().use { arena ->
+            val native = desktop_windows_h.renderer_angle_get_egl_get_proc_func(arena, angleDevicePtr)
+            val f = NativeEglGetProcFuncData.f(native)
+            val ctx = NativeEglGetProcFuncData.ctx(native)
+            EglGetProcFunc(f.address(), ctx.address())
+        }
+    }
+
+    public fun makeSurface(width: Int, height: Int) {
+        ffiDownCall {
+            desktop_windows_h.renderer_angle_make_surface(angleDevicePtr, width, height)
+        }
+    }
+
+    public fun draw(waitForVsync: Boolean, drawFun: () -> Unit) {
+        Arena.ofConfined().use { arena ->
+            val callbacks = NativeAngleDeviceCallbacks.allocate(arena)
+            NativeAngleDeviceCallbacks.draw_fun(callbacks, NativeAngleDeviceDrawFun.allocate(drawFun, arena))
+            desktop_windows_h.renderer_angle_draw(angleDevicePtr, waitForVsync, callbacks)
+        }
+    }
+
+    override fun close() {
+        ffiDownCall {
+            desktop_windows_h.renderer_angle_drop(angleDevicePtr)
+        }
+    }
+}
