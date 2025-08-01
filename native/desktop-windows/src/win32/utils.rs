93b54ae1118953c6b167ba25f2fd78b537083f43
Alovchin91 <8490695+Alovchin91@users.noreply.github.com>
2025-08-01 18:10:59 +0200

    Refactor the event loop

--- native/desktop-windows/src/win32/utils.rs
+++ native/desktop-windows/src/win32/utils.rs
@@ -1,35 +1,19 @@
-use windows::Win32::Foundation::{LPARAM, WPARAM};
-
-pub(crate) trait WLParamUtil {
-    type T;
-
-    #[allow(non_snake_case)]
-    fn LOWORD(&self) -> Self::T;
-
-    #[allow(non_snake_case)]
-    fn HIWORD(&self) -> Self::T;
+macro_rules! LOWORD {
+    ($arg:ident) => {
+        ($arg.0 & 0xffff)
+    };
 }
 
-impl WLParamUtil for WPARAM {
-    type T = usize;
-
-    fn LOWORD(&self) -> Self::T {
-        self.0 & 0xffff
-    }
-
-    fn HIWORD(&self) -> Self::T {
-        (self.0 >> 16) & 0xffff
-    }
+macro_rules! HIWORD {
+    ($arg:ident) => {
+        (($arg.0 >> 16) & 0xffff)
+    };
 }
 
-impl WLParamUtil for LPARAM {
-    type T = isize;
-
-    fn LOWORD(&self) -> Self::T {
-        self.0 & 0xffff
-    }
-
-    fn HIWORD(&self) -> Self::T {
-        (self.0 >> 16) & 0xffff
-    }
+macro_rules! GET_Y_LPARAM {
+    ($arg:ident) => {
+        ((($arg.0 >> 16) & 0xffff) as i16)
+    };
 }
+
+pub(crate) use {GET_Y_LPARAM, HIWORD, LOWORD};
