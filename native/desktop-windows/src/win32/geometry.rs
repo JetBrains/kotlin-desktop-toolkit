e3e569fa59159a70958ee39f7770bb0dc6e9d2a9
Alovchin91 <8490695+Alovchin91@users.noreply.github.com>
2025-08-01 18:10:56 +0200

    HiDPI

--- native/desktop-windows/src/win32/geometry.rs
+++ native/desktop-windows/src/win32/geometry.rs
@@ -13,11 +13,29 @@ pub struct PhysicalPoint {
     pub y: PhysicalPixels,
 }
 
+impl PhysicalPoint {
+    pub fn new(x: i32, y: i32) -> Self {
+        Self {
+            x: PhysicalPixels(x),
+            y: PhysicalPixels(y),
+        }
+    }
+}
+
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
 pub struct PhysicalSize {
     pub width: PhysicalPixels,
     pub height: PhysicalPixels,
+}
+
+impl PhysicalSize {
+    pub fn new(width: i32, height: i32) -> Self {
+        Self {
+            width: PhysicalPixels(width),
+            height: PhysicalPixels(height),
+        }
+    }
 }
 
 #[repr(C)]
