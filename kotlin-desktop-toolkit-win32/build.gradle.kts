3c7043b789b54dde53b9a2ae9389ce1de2f924db
Nikola Kocić <nikola.kocic@jetbrains.com>
2025-08-05 13:15:55 +0200

    build: fix kotlin-desktop-toolkit:assemble not producing native libraries
    Now the native libraries are also properly recompiled when running a sample app

--- kotlin-desktop-toolkit-win32/build.gradle.kts
+++ kotlin-desktop-toolkit-win32/build.gradle.kts
@@ -134,14 +134,6 @@ val generateBindingsTaskByOS = allPlatforms.allOSes().associateWith { os ->
     }
 }
 
-tasks.compileJava {
-    generateBindingsTaskByOS.values.forEach { dependsOn(it) }
-}
-
-tasks.compileKotlin {
-    generateBindingsTaskByOS.values.forEach { dependsOn(it) }
-}
-
 sourceSets.main {
     generateBindingsTaskByOS.values.forEach { task ->
         java.srcDir(task.flatMap { it.generatedSourcesDirectory })
@@ -179,6 +171,16 @@ val nativeJarTasksByPlatform = enabledPlatforms.associateWith { platform ->
             from(compileTask.flatMap { it.libraryFile })
         }
     }
+}
+
+tasks.compileJava {
+    dependsOn(nativeJarTasksByPlatform.values)
+    dependsOn(generateBindingsTaskByOS.values)
+}
+
+tasks.compileKotlin {
+    dependsOn(nativeJarTasksByPlatform.values)
+    dependsOn(generateBindingsTaskByOS.values)
 }
 
 val spaceUsername: String? by project
