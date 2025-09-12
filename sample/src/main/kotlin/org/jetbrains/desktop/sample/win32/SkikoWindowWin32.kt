65dd5361c96bedffcfb7c8eeac11e80bf4296bac
Alovchin91 <8490695+Alovchin91@users.noreply.github.com>
2025-09-12 17:22:33 +0200

    fix(win32): Use and recreate Skia surface on every draw cycle
    This fixes random crashes with illegal instruction exception in DirectContext._nFlushAndSubmit

--- sample/src/main/kotlin/org/jetbrains/desktop/sample/win32/SkikoWindowWin32.kt
+++ sample/src/main/kotlin/org/jetbrains/desktop/sample/win32/SkikoWindowWin32.kt
@@ -6,9 +6,11 @@ import org.jetbrains.desktop.win32.Event
 import org.jetbrains.desktop.win32.EventHandlerResult
 import org.jetbrains.desktop.win32.LogicalSize
 import org.jetbrains.desktop.win32.PhysicalSize
+import org.jetbrains.desktop.win32.SurfaceParams
 import org.jetbrains.desktop.win32.WindowParams
 import org.jetbrains.skia.BackendRenderTarget
 import org.jetbrains.skia.Canvas
+import org.jetbrains.skia.Color
 import org.jetbrains.skia.ColorSpace
 import org.jetbrains.skia.DirectContext
 import org.jetbrains.skia.FramebufferFormat
@@ -37,7 +39,7 @@ abstract class SkikoWindowWin32(
     private val creationTime = TimeSource.Monotonic.markNow()
 
     private var currentSize = PhysicalSize(0, 0)
-    private var surface: Surface? = null
+    private var surfaceParams: SurfaceParams? = null
 
     init {
         window.setMinSize(LogicalSize(320.0f, 240.0f))
@@ -49,19 +51,20 @@ abstract class SkikoWindowWin32(
 
     open fun handleEvent(event: Event): EventHandlerResult {
         return when (event) {
-            is Event.WindowDraw -> {
-                performDrawing(event.size, event.scale)
+            is Event.WindowDraw -> with(event) {
+                performDrawing(size, scale)
                 EventHandlerResult.Stop
             }
 
-            is Event.WindowScaleChanged -> {
-                window.setRect(event.origin, event.size)
+            is Event.WindowScaleChanged -> with(event) {
+                window.setRect(origin, size)
                 EventHandlerResult.Stop
             }
 
-            is Event.WindowPositionChanging -> {
-                if (isSizeChanged(event.size)) {
-                    makeSurface(event.size, event.scale)
+            is Event.WindowPositionChanging -> with(event) {
+                if (surfaceParams == null || isSizeChanged(size)) {
+                    currentSize = size
+                    surfaceParams = angleRenderer.resizeSurface(size.width, size.height)
                     angleRenderer.swapBuffers()
                 }
                 EventHandlerResult.Stop
@@ -72,24 +75,12 @@ abstract class SkikoWindowWin32(
     }
 
     private fun performDrawing(size: PhysicalSize, scale: Float) {
-        surface!!.let { surface ->
-            val time = creationTime.elapsedNow().inWholeMilliseconds
-            angleRenderer.draw {
-                surface.canvas.draw(size, scale, time)
-                surface.flushAndSubmit()
-            }
-        }
-    }
-
-    private fun makeSurface(size: PhysicalSize, scale: Float) {
-        currentSize = size
-        val surfaceParams = angleRenderer.resizeSurface(size.width, size.height)
-        surface = BackendRenderTarget.makeGL(
-            width = size.width,
-            height = size.height,
+        BackendRenderTarget.makeGL(
+            width = currentSize.width,
+            height = currentSize.height,
             sampleCnt = 1,
             stencilBits = 8,
-            fbId = surfaceParams.framebufferBinding,
+            fbId = surfaceParams!!.framebufferBinding,
             fbFormat = FramebufferFormat.GR_GL_RGBA8,
         ).use { renderTarget ->
             Surface.makeFromBackendRenderTarget(
@@ -99,7 +90,14 @@ abstract class SkikoWindowWin32(
                 colorFormat = SurfaceColorFormat.RGBA_8888,
                 colorSpace = ColorSpace.sRGB,
                 surfaceProps = null,
-            )
+            )!!.use { surface ->
+                val time = creationTime.elapsedNow().inWholeMilliseconds
+                angleRenderer.draw {
+                    surface.canvas.clear(Color.TRANSPARENT)
+                    surface.canvas.draw(size, scale, time)
+                    surface.flushAndSubmit()
+                }
+            }
         }
     }
 
