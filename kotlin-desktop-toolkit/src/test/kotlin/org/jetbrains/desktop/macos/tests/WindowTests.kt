832fd91481a70d8b675b14c64249fefb5038b710
pavel.sergeev <sergeev.pavel.al@gmail.com>
2025-11-24 16:26:54 +0100

    add test to verify that window is created with required size

--- kotlin-desktop-toolkit/src/test/kotlin/org/jetbrains/desktop/macos/tests/WindowTests.kt
+++ kotlin-desktop-toolkit/src/test/kotlin/org/jetbrains/desktop/macos/tests/WindowTests.kt
@@ -1,10 +1,13 @@
 package org.jetbrains.desktop.macos.tests
 
 import org.jetbrains.desktop.macos.LogicalPoint
+import org.jetbrains.desktop.macos.LogicalSize
+import org.jetbrains.desktop.macos.TitlebarConfiguration
 import org.jetbrains.desktop.macos.Window
 import org.junit.jupiter.api.condition.EnabledOnOs
 import org.junit.jupiter.api.condition.OS
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
@@ -42,6 +45,37 @@ class WindowTests : KDTApplicationTestBase() {
 
         ui {
             window.close()
+        }
+    }
+
+    @Test
+    fun `window is created with expected size`() {
+        val windowWithCustomTitlebar = ui {
+            Window.create(
+                title = "Window with custom titlebar",
+                size = LogicalSize(100.0, 100.0),
+                titlebarConfiguration = TitlebarConfiguration.Custom(titlebarHeight = 10.0),
+                isResizable = false
+            )
+        }
+
+        val windowWithRegularTitlebar = ui {
+            Window.create(
+                title = "Window with regular titlebar",
+                size = LogicalSize(100.0, 100.0),
+                titlebarConfiguration = TitlebarConfiguration.Regular,
+                isResizable = false
+            )
+        }
+
+        ui {
+            assertEquals(windowWithCustomTitlebar.size, LogicalSize(100.0, 100.0), "A custom titlebar window should have the expected size")
+            assertEquals(windowWithRegularTitlebar.size, LogicalSize(100.0, 100.0), "Regular titlebar window should have the expected size")
+        }
+
+        ui {
+            windowWithCustomTitlebar.close()
+            windowWithRegularTitlebar.close()
         }
     }
 }
