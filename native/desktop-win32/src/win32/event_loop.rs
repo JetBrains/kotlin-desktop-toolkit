0eff6de4fe07deeca5962a0201ad4792827ea70d
Alovchin91 <8490695+Alovchin91@users.noreply.github.com>
2025-12-24 12:25:56 +0100

    chore(win32): Code style

--- native/desktop-win32/src/win32/event_loop.rs
+++ native/desktop-win32/src/win32/event_loop.rs
@@ -206,16 +206,13 @@ fn on_windowposchanged(event_loop: &EventLoop, window: &Window, lparam: LPARAM) 
 }
 
 fn on_getminmaxinfo(window: &Window, lparam: LPARAM) -> Option<LRESULT> {
-    if let Some(min_max_info) = unsafe { (lparam.0 as *mut MINMAXINFO).as_mut() }
-        && let Some(min_size) = window.get_min_size()
-    {
-        let scale = window.get_scale();
-        let physical_size = min_size.to_physical(scale);
-        min_max_info.ptMinTrackSize.x = physical_size.width.0;
-        min_max_info.ptMinTrackSize.y = physical_size.height.0;
-        return Some(LRESULT(0));
-    }
-    None
+    let min_max_info = unsafe { (lparam.0 as *mut MINMAXINFO).as_mut() }?;
+    let min_size = window.get_min_size()?;
+    let scale = window.get_scale();
+    let physical_size = min_size.to_physical(scale);
+    min_max_info.ptMinTrackSize.x = physical_size.width.0;
+    min_max_info.ptMinTrackSize.y = physical_size.height.0;
+    Some(LRESULT(0))
 }
 
 #[allow(clippy::cast_possible_truncation)]
@@ -279,26 +276,27 @@ fn on_activate(window: &Window) -> Option<LRESULT> {
 }
 
 fn on_nccalcsize(event_loop: &EventLoop, window: &Window, wparam: WPARAM, lparam: LPARAM) -> Option<LRESULT> {
-    if wparam.0 == windows::Win32::Foundation::TRUE.0 as usize
-        && let Some(calcsize_params) = unsafe { (lparam.0 as *mut NCCALCSIZE_PARAMS).as_mut() }
-    {
-        let top = calcsize_params.rgrc[0].top;
-        let result = unsafe { DefWindowProcW(window.hwnd(), WM_NCCALCSIZE, wparam, lparam) };
-        if window.has_custom_title_bar() && result.0 == 0 {
-            // the top inset should be 0 otherwise Windows will draw full native title bar
-            calcsize_params.rgrc[0].top = top;
-        }
-        let origin = PhysicalPoint::new(calcsize_params.rgrc[0].left, calcsize_params.rgrc[0].top);
-        let size = PhysicalSize::new(
-            calcsize_params.rgrc[0].right - calcsize_params.rgrc[0].left,
-            calcsize_params.rgrc[0].bottom - calcsize_params.rgrc[0].top,
-        );
-        let scale = window.get_scale();
-        let event = NCCalcSizeEvent { origin, size, scale };
-        event_loop.handle_event(window, event.into());
-        return Some(result);
+    // Documentation (https://learn.microsoft.com/en-us/windows/win32/winmsg/wm-nccalcsize):
+    //   "If wParam is FALSE, the application does not need to indicate the valid part of the client area."
+    if wparam.0 != windows::Win32::Foundation::TRUE.0 as usize {
+        return None;
     }
-    None
+    let calcsize_params = unsafe { (lparam.0 as *mut NCCALCSIZE_PARAMS).as_mut() }?;
+    let top = calcsize_params.rgrc[0].top;
+    let result = unsafe { DefWindowProcW(window.hwnd(), WM_NCCALCSIZE, wparam, lparam) };
+    if window.has_custom_title_bar() && result.0 == 0 {
+        // the top inset should be 0 otherwise Windows will draw full native title bar
+        calcsize_params.rgrc[0].top = top;
+    }
+    let origin = PhysicalPoint::new(calcsize_params.rgrc[0].left, calcsize_params.rgrc[0].top);
+    let size = PhysicalSize::new(
+        calcsize_params.rgrc[0].right - calcsize_params.rgrc[0].left,
+        calcsize_params.rgrc[0].bottom - calcsize_params.rgrc[0].top,
+    );
+    let scale = window.get_scale();
+    let event = NCCalcSizeEvent { origin, size, scale };
+    event_loop.handle_event(window, event.into());
+    Some(result)
 }
 
 #[allow(clippy::cast_lossless)]
