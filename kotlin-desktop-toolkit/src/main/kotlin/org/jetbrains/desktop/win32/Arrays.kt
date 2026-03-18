8d1ed064f6bf53692333b901d0f19e44a67f5fc3
Alovchin91 <8490695+Alovchin91@users.noreply.github.com>
2026-03-18 14:59:21 +0100

    feat(win32): Infallible Clipboard API

--- kotlin-desktop-toolkit/src/main/kotlin/org/jetbrains/desktop/win32/Arrays.kt
+++ kotlin-desktop-toolkit/src/main/kotlin/org/jetbrains/desktop/win32/Arrays.kt
@@ -1,20 +1,36 @@
 package org.jetbrains.desktop.win32
 
+import org.jetbrains.desktop.macos.generated.NativeBorrowedArray_u8
 import org.jetbrains.desktop.win32.generated.NativeAutoDropArray_u32
 import org.jetbrains.desktop.win32.generated.NativeAutoDropArray_u8
+import org.jetbrains.desktop.win32.generated.NativeFfiOption_AutoDropByteArray
+import java.lang.foreign.Arena
 import java.lang.foreign.MemorySegment
 import java.lang.foreign.ValueLayout.JAVA_BYTE
 import java.lang.foreign.ValueLayout.JAVA_INT
 
-internal fun byteArrayFromNative(segment: MemorySegment): ByteArray? {
+internal fun ByteArray.toNative(arena: Arena): MemorySegment = let { bytes ->
+    val result = NativeBorrowedArray_u8.allocate(arena)
+    NativeBorrowedArray_u8.ptr(result, arena.allocateArray(JAVA_BYTE, *bytes))
+    NativeBorrowedArray_u8.len(result, bytes.count().toLong())
+    result
+}
+
+internal fun byteArrayFromNative(segment: MemorySegment): ByteArray {
     val ptr = NativeAutoDropArray_u8.ptr(segment)
     val len = NativeAutoDropArray_u8.len(segment)
-    if (ptr == MemorySegment.NULL) {
-        return null
-    }
     val result = ByteArray(len.toInt())
     MemorySegment.copy(ptr, JAVA_BYTE, 0, result, 0, result.size)
     return result
+}
+
+internal fun optionalByteArrayFromNative(segment: MemorySegment): ByteArray? {
+    val value = NativeFfiOption_AutoDropByteArray.value(segment)
+    val isSome = NativeFfiOption_AutoDropByteArray.is_some(segment)
+    if (!isSome) {
+        return null
+    }
+    return byteArrayFromNative(value)
 }
 
 internal fun intArrayFromNative(segment: MemorySegment): IntArray {
