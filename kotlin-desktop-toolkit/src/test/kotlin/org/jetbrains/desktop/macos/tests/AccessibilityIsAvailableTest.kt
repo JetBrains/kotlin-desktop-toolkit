791f734efca07b5116662873f62a743d00445d59
pavel.sergeev <sergeev.pavel.al@gmail.com>
2026-04-14 15:02:38 +0200

    test(macos): add a separate test to check that accessibility is available

---
+++ kotlin-desktop-toolkit/src/test/kotlin/org/jetbrains/desktop/macos/tests/AccessibilityIsAvailableTest.kt
@@ -0,0 +1,20 @@
+package org.jetbrains.desktop.macos.tests
+
+import org.jetbrains.desktop.macos.Robot
+import org.junit.jupiter.api.condition.EnabledOnOs
+import org.junit.jupiter.api.condition.OS
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+@EnabledOnOs(OS.MAC)
+class AccessibilityIsAvailableTest: KDTApplicationTestBase() {
+    @Test
+    fun `Accessibility is available`() {
+        val robot = ui { Robot() }
+        try {
+            assertTrue { robot.isAccessibilityAllowed() }
+        } finally {
+            ui { robot.close() }
+        }
+    }
+}
