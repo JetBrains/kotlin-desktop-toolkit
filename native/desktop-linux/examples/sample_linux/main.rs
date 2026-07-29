b4d1b98769f0982efd5ed51a23f7bec242ee03e1
Nikola Kocić <nikola.kocic@jetbrains.com>
2026-07-29 16:15:54 +0200

    feat(linux): native skia sample

--- native/desktop-linux/examples/sample_linux/main.rs
+++ native/desktop-linux/examples/sample_linux/main.rs
@@ -7,6 +7,12 @@ mod sample_linux_actions;
 #[cfg(target_os = "linux")]
 mod sample_linux_draw;
 
+#[cfg(all(target_os = "linux", feature = "skia"))]
+mod sample_linux_draw_skia;
+
+#[cfg(target_os = "linux")]
+mod sample_linux_draw_software;
+
 #[cfg(target_os = "linux")]
 pub fn main() {
     sample_linux::main();
