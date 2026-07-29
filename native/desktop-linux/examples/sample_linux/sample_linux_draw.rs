c7241cf194692e1df2241486946c806cc153a326
Nikola Kocić <nikola.kocic@jetbrains.com>
2026-07-29 16:15:54 +0200

    feat(linux): optimize software rendering in sample app

--- native/desktop-linux/examples/sample_linux/sample_linux_draw.rs
+++ native/desktop-linux/examples/sample_linux/sample_linux_draw.rs
@@ -20,6 +20,7 @@ pub struct OpenglState {
     programs: HashMap<WindowId, GLuint>,
 }
 
+const BYTES_PER_PIXEL: usize = 4;
 const V_POSITION: GLuint = 0;
 const DRAG_AND_DROP_LEFT_OF: f64 = 100.;
 
@@ -127,94 +128,112 @@ pub fn draw_opengl_triangle_with_init(app_ptr: AppPtr, physical_size: PhysicalSi
     draw_opengl_triangle(&opengl_state.gl, *program, physical_size, animation_progress);
 }
 
+#[allow(clippy::cast_sign_loss, clippy::cast_possible_truncation)]
+const fn pixel_bytes(v: f64) -> usize {
+    v.round() as usize * BYTES_PER_PIXEL
+}
+
+fn stride_len(v: u32, stride: i32) -> usize {
+    usize::try_from(v * stride.unsigned_abs()).unwrap()
+}
+
+fn stride_offset(v: i32, stride: i32) -> isize {
+    isize::try_from(v * stride).unwrap()
+}
+
 #[allow(clippy::many_single_char_names)]
-pub fn draw_software(data: &SoftwareDrawData, physical_size: PhysicalSize, scale: f64, window_state: &WindowState) {
-    const BYTES_PER_PIXEL: u8 = 4;
-    let drag_source_indicator_heigh = 100. * scale;
-    let canvas = {
-        let len = usize::try_from(physical_size.height.0 * data.stride).unwrap();
-        unsafe { std::slice::from_raw_parts_mut(data.canvas, len) }
-    };
+pub fn draw_software(data: &SoftwareDrawData, physical_size: PhysicalSize, window_state: &WindowState) {
+    let scale = window_state.scale;
+    let stride = data.stride;
     let w = f64::from(physical_size.width.0);
     let h = f64::from(physical_size.height.0);
-    let line_thickness = 5.0 * scale;
+
+    let line_thickness = 4. * scale;
+    let drag_and_drop_left_of = DRAG_AND_DROP_LEFT_OF * scale;
+    let drag_source_indicator_height = 100. * scale;
+
+    let mut horizontal_line = vec![0; stride_len(1, stride)];
 
     // Order of bytes in `pixel` is [b, g, r, a] (for the Argb8888 format)
-    for (pixel, i) in canvas.chunks_exact_mut(BYTES_PER_PIXEL.into()).zip(1u32..) {
-        let i = f64::from(i);
-        let x = i % w;
-        let y = (i / f64::from(data.stride)) * f64::from(BYTES_PER_PIXEL);
-        if between(
-            x,
-            DRAG_AND_DROP_LEFT_OF * scale,
-            DRAG_AND_DROP_LEFT_OF.mul_add(scale, line_thickness),
-        ) {
-            pixel[0] = 0;
-            pixel[1] = 0;
-            pixel[2] = 0;
-        } else if between(x, line_thickness,  line_thickness * 2.0)  // left border
-           || between(y, line_thickness,  line_thickness * 2.0)  // top border
-           || between(x, line_thickness.mul_add(-2.0, w), w - line_thickness)  // right border
-           || between(y, line_thickness.mul_add(-2.0, h), h - line_thickness)  // bottom border
-           || between(x, (i / h) - (line_thickness / 2.0), (i / h) + (line_thickness / 2.0))
-        {
-            pixel[0] = 0;
-            pixel[1] = 0;
-            pixel[2] = 255;
-        } else if x < DRAG_AND_DROP_LEFT_OF
-            && window_state.drag_and_drop_source
-            && between(y, drag_source_indicator_heigh, drag_source_indicator_heigh + line_thickness)
-        {
-            pixel[0] = 255;
-            pixel[1] = 0;
-            pixel[2] = 0;
-        } else if x < DRAG_AND_DROP_LEFT_OF && window_state.drag_and_drop_target {
-            pixel[0] = 128;
-            pixel[1] = 0;
-            pixel[2] = 0;
+    for (pixel, i) in horizontal_line.as_chunks_mut::<BYTES_PER_PIXEL>().0.iter_mut().zip(0u32..) {
+        let x = f64::from(i % physical_size.width.0.cast_unsigned());
+        if between(x, drag_and_drop_left_of, drag_and_drop_left_of + line_thickness) {
+            *pixel = [0, 0, 0, 255];
+        } else if between(x, line_thickness, line_thickness * 2.) || between(x, w - (line_thickness * 2.), w - line_thickness) {
+            // left and right border
+            *pixel = [0, 0, 255, 255];
+        } else if x < drag_and_drop_left_of && window_state.drag_and_drop_target {
+            *pixel = [128, 0, 0, 255];
         } else if window_state.active {
-            pixel[0] = 255;
-            pixel[1] = 255;
-            pixel[2] = 255;
+            pixel.fill(255);
         } else {
-            pixel[0] = 128;
-            pixel[1] = 128;
-            pixel[2] = 128;
+            *pixel = [128, 128, 128, 255];
         }
-        pixel[3] = 255;
+    }
+
+    let content_x_end_bytes = pixel_bytes(w);
+    let half_line_thickness = line_thickness / 2.;
+
+    for y in 0..physical_size.height.0 {
+        let line = unsafe { std::slice::from_raw_parts_mut(data.canvas.offset(stride_offset(y, stride)), pixel_bytes(w)) };
+        line.copy_from_slice(&horizontal_line);
+
+        let y = f64::from(y);
+
+        if between(y, line_thickness, line_thickness * 2.) || between(y, h - (line_thickness * 2.), h - line_thickness) {
+            // top and bottom border
+            line[..content_x_end_bytes]
+                .as_chunks_mut::<BYTES_PER_PIXEL>()
+                .0
+                .fill([0, 0, 255, 255]);
+        }
+
+        if window_state.drag_and_drop_source && between(y, drag_source_indicator_height, drag_source_indicator_height + line_thickness) {
+            line[..pixel_bytes(drag_and_drop_left_of)]
+                .as_chunks_mut::<BYTES_PER_PIXEL>()
+                .0
+                .fill([255, 0, 0, 255]);
+        }
+
+        let diagonal_x = (y * w) / h;
+
+        let diagonal_start_x_pixel = pixel_bytes((diagonal_x - half_line_thickness).max(0.));
+        let diagonal_end_x_pixel = pixel_bytes(diagonal_x + half_line_thickness).min(content_x_end_bytes);
+        line[diagonal_start_x_pixel..diagonal_end_x_pixel]
+            .as_chunks_mut::<BYTES_PER_PIXEL>()
+            .0
+            .fill([0, 0, 255, 255]);
     }
 }
 
 #[allow(clippy::many_single_char_names)]
 pub fn draw_software_drag_icon(data: &SoftwareDrawData, physical_size: PhysicalSize, scale: f64) {
-    const BYTES_PER_PIXEL: u8 = 4;
-    let canvas = {
-        let len = usize::try_from(physical_size.height.0 * data.stride).unwrap();
-        unsafe { std::slice::from_raw_parts_mut(data.canvas, len) }
-    };
+    let stride = data.stride;
     let w = f64::from(physical_size.width.0);
     let h = f64::from(physical_size.height.0);
-    let line_thickness = 5.0 * scale;
+
+    let line_thickness = 4. * scale;
+
+    let mut horizontal_line = vec![0; stride_len(1, stride)];
 
     // Order of bytes in `pixel` is [b, g, r, a] (for the Argb8888 format)
-    for (pixel, i) in canvas.chunks_exact_mut(BYTES_PER_PIXEL.into()).zip(1u32..) {
-        let i = f64::from(i);
-        let x = i % w;
-        let y = (i / f64::from(data.stride)) * f64::from(BYTES_PER_PIXEL);
-
-        if between(x, line_thickness,  line_thickness * 2.0)  // left border
-           || between(y, line_thickness,  line_thickness * 2.0)  // top border
-           || between(x, line_thickness.mul_add(-2.0, w), w - line_thickness)  // right border
-           || between(y, line_thickness.mul_add(-2.0, h), h - line_thickness)  // bottom border
-           || between(x, (i / h) - (line_thickness / 2.0), (i / h) + (line_thickness / 2.0))
-        {
-            pixel[0] = 0;
-            pixel[1] = 0;
-        } else {
-            pixel[0] = 128;
-            pixel[1] = 128;
+    for (pixel, i) in horizontal_line.as_chunks_mut::<BYTES_PER_PIXEL>().0.iter_mut().zip(0u32..) {
+        let x = f64::from(i % physical_size.width.0.cast_unsigned());
+        if between(x, line_thickness, line_thickness * 2.) || between(x, w - (line_thickness * 2.), w - line_thickness) {
+            // left and right border
+            *pixel = [0, 0, 255, 255];
         }
-        pixel[2] = 128;
-        pixel[3] = 128;
+    }
+
+    for y in 0..physical_size.height.0 {
+        let line = unsafe { std::slice::from_raw_parts_mut(data.canvas.offset(stride_offset(y, stride)), pixel_bytes(w)) };
+        let y = f64::from(y);
+
+        if between(y, line_thickness, line_thickness * 2.) || between(y, h - (line_thickness * 2.), h - line_thickness) {
+            // top and bottom border
+            line.as_chunks_mut::<BYTES_PER_PIXEL>().0.fill([0, 0, 255, 255]);
+        } else {
+            line.copy_from_slice(&horizontal_line);
+        }
     }
 }
